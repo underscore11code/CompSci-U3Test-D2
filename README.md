13da441b264abd0cdc093b7e01bf7df6a16d1552
underscore11code <minecrafter11mrt@gmail.com>
2021-03-19 10:04:31 -0700

    Code.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # CompSci
 A bunch of Java for usage in my Comp Sci class. Expect messy code.
+
+Rainforest Prime offers a discount to customers depending on the amount of their purchase before taxes. Write the code only.
+
+Purchase			Discount
+
+* 0 - 99.99			5%
+
+* 100.00 – 499.99		10%
+
+* 500.00 – 999.99		15%
+
+* over 999.99			25%
+
+Write a program that prompts the user for money they spent and displays the appropriate discount. Use the percentage format from the NumberFormat class. Use switch-case statement for bonus marks.
+
+## A Program Run Sketch:
+
+Enter the amount of your purchases:
+
+**350**
+
+Your discount before taxes is 10% 
--- src/main/java/io/github/underscore11code/compsci/Main.java
+++ src/main/java/io/github/underscore11code/compsci/Main.java
@@ -1,7 +1,28 @@
 package io.github.underscore11code.compsci;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    // Do shit here
+    final Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the amount of your purchases: ");
+    final double originalCost = scanner.nextDouble();
+    scanner.close();
+    double deduction = 0;
+    if (originalCost >= 1000)
+      deduction = 0.25;
+    else if (inRange(originalCost, 500, 999.99))
+      deduction = 0.15;
+    else if (inRange(originalCost, 100, 499.99))
+      deduction = 0.10;
+    else if (inRange(originalCost, 0, 99.99))
+      deduction = 0.05;
+
+    System.out.println("Your deduction is " + NumberFormat.getPercentInstance().format(deduction));
+  }
+
+  private static boolean inRange(final double original, final double lower, final double upper) {
+    return original >= lower && original <= upper;
   }
 }
